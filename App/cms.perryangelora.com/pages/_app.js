bd6e42fb73e0f8079eb96096919ca742c1c84363
Perry Angelora <peamaillist@gmail.com>
2022-04-17 12:43:58 -0400

    Commit to preserve working state

--- App/cms.perryangelora.com/pages/_app.js
+++ App/cms.perryangelora.com/pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/main.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <>
+      <Head>
+        <title>perryangelora.com cms</title> 
+        <meta property='og:title' content='Perry Angelora - Painter :: Printmaker - CMS' key="title" />
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1 viewport-fit=cover" />
+
+      </Head>
+      <Component {...pageProps} />
+    </>);
 }
 
-export default MyApp
+export default MyApp;
