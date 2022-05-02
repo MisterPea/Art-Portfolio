8546b5d2507250fbd1d492fc6aa976aec63b605f
Perry Angelora <peamaillist@gmail.com>
2022-05-02 16:11:56 -0400

    Setting up - initial commit

--- App/perryangelora.com/pages/_app.js
+++ App/perryangelora.com/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Navbar from '../src/Navbar';
+import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Navbar />
+    <Component {...pageProps} />
+  </>);
 }
 
-export default MyApp
+export default MyApp;
