73601256d9533600a5e411dabdc8ccfc4d798dcd
Perry Angelora <peamaillist@gmail.com>
2022-05-13 17:57:05 -0400

    Removed old version off app

--- App/cms.perryangelora.com/src/components/CircleButton.js
+++
@@ -1,6 +0,0 @@
-import { VscChromeClose } from 'react-icons/vsc';
-export default function CircleButton({ clickHandle }) {
-  return (
-    <VscChromeClose onClick={clickHandle} className='circle-button' />
-  );
-}
--- App/cms.perryangelora.com/src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default function Navbar() {
-  return (
-    <h1>Perry<br />Angelora</h1>
-  )
-}
