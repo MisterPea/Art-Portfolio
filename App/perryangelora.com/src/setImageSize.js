2c2d342ce91840b48e9150e5ab6124b70643b0c9
Perry Angelora <peamaillist@gmail.com>
2022-05-12 21:44:11 -0400

    Added an addition computation for more image size/window size possibilities

--- App/perryangelora.com/src/setImageSize.js
+++ App/perryangelora.com/src/setImageSize.js
@@ -1,3 +1,10 @@
+/**
+ * Utility method to determine scaling of image as determined by 
+ * the aspect ration of the viewport.
+ * @param {Number} imgH 1 or a 1 + a decimal representing the ratio of H to W
+ * @param {Number} imgW 1 or a 1 + a decimal representing the ratio of W to H
+ * @returns Returns an object
+ */
 const setImageSize = (imgH, imgW) => {
   // imgH, imgW is a ratio
   // get frame size and ratio
@@ -5,7 +12,8 @@ const setImageSize = (imgH, imgW) => {
   const frameRatio = { h: 1, w: 1 };
   const frameHeight = window.innerHeight;
   const frameWidth = window.innerWidth;
-  const shrink = 0.95;
+  const largeShrink = 0.90;
+  const smallShrink = 0.95;
 
   if(frameHeight > frameWidth) {
     frameRatio.h = frameHeight / frameWidth;
@@ -15,24 +23,32 @@ const setImageSize = (imgH, imgW) => {
   
   if(frameRatio.h === 1) {
     // horizontal frame
-    // horizontal image
-    if(imgH === 1 && imgW <= frameRatio.w) {
-      imageSizeSet.h = frameHeight * shrink;
-      imageSizeSet.w = imageSizeSet.h * imgW;
-    } else {
-      // vertical image
-      imageSizeSet.h = frameHeight * shrink;
+    if(imgH === 1 && frameRatio.w > imgW) {
+      imageSizeSet.h = frameHeight * smallShrink;
       imageSizeSet.w = imgW / imgH * imageSizeSet.h;
-    }
-  } else { 
-    // vertical frame
-    // vertical image
-    if(imgW === 1 && imgH <= frameRatio.h) {
-      imageSizeSet.w = frameWidth * shrink;
-      imageSizeSet.h = imageSizeSet.w * imgH;
+    } else if(imgW === 1) {
+      imageSizeSet.h = frameHeight * smallShrink;
+      imageSizeSet.w = imgW / imgH * imageSizeSet.h;
     } else {
-      imageSizeSet.w = frameWidth * shrink;
-      imageSizeSet.h = imgH / imgW * imageSizeSet.w;
+      imageSizeSet.w = frameWidth * smallShrink;
+      imageSizeSet.h = imgH/imgW * imageSizeSet.w;
+      // imageSizeSet.h = frameHeight * smallShrink;
+      // imageSizeSet.w = imageSizeSet.h * imgW;
+    }
+  } else if(frameRatio.w === 1){ 
+    // vertical frame
+    //vertical image
+    if(imgW === 1 && frameRatio.h > imgH) {
+      imageSizeSet.w = frameWidth * smallShrink;
+      imageSizeSet.h = imgH /imgW * imageSizeSet.w;
+      // horizontal image
+    } else if(imgH === 1) {
+      imageSizeSet.w = frameWidth * smallShrink;
+      imageSizeSet.h = imgH/imgW * imageSizeSet.w;
+      // large vertical image
+    } else {
+      imageSizeSet.h = frameHeight * largeShrink;
+      imageSizeSet.w = imgW / imgH * imageSizeSet.h;
     }
   }
   return { h:Math.ceil(imageSizeSet.h), w: Math.ceil(imageSizeSet.w) };
