5920443aa118a3574158f2ed9bf3bb5c05bbb66e
Perry Angelora <peamaillist@gmail.com>
2022-05-12 21:47:39 -0400

    Pushed routes to '/' over to '/monochrome'

--- App/perryangelora.com/pages/index.js
+++ App/perryangelora.com/pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Navbar from '../src/Navbar';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/monochrome', undefined, { shallow: true });
+  });
+
   return (
-    <div>
-      
-      
-    </div>
+    <div />
   );
 }
