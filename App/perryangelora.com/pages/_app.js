17526f5b5ac0bfda0403cf142c0522171d64d621
Perry Angelora <peamaillist@gmail.com>
2022-05-12 21:48:01 -0400

    Footer added

--- App/perryangelora.com/pages/_app.js
+++ App/perryangelora.com/pages/_app.js
@@ -2,15 +2,26 @@ import Navbar from '../src/Navbar';
 import '../styles/main.scss';
 import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   const { route } = useRouter();
+  const year = new Date().getFullYear();
+
 
   return (<>
-    <Navbar />
-    <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} key={route} />
-    </AnimatePresence>
+    <div className='main-body'>
+      <Navbar />
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={route} />
+      </AnimatePresence>
+      <footer>
+        <Link href='/about' >
+          <a style={{ display: `${route === '/about' ? 'none':'block'}` }} className='about-bottom'>About</a>
+        </Link>
+        <div className='date'>{`©${year} Perry Angelora`}</div>
+      </footer>
+    </div>
   </>);
 }
 
